a5ecaff0fa11551e414f6f18a9f5b3bb15dda8d7
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-04-16 00:58:19 +0300

    g-tag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,14 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxt/ui'],
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxt/ui', 'nuxt-gtag'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
       tailwindcss(),
     ],
+  },
+  gtag: {
+    id: 'G-4R3XKFL775',
   },
 })
