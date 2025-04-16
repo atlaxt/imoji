fe7458aaad389a8cae32344b40515961dc7c509f
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-04-16 16:13:50 +0300

    loading

--- components/welcome.vue
+++ components/welcome.vue
@@ -142,14 +142,16 @@ onMounted(() => {
       <div
         v-for="group in groups"
         :key="group.key"
-        class="group cursor-pointer lg:p-3 p-1 lg:whitespace-normal whitespace-nowrap flex lg:flex-col flex-row items-center justify-center gap-1
-    rounded-lg border border-zinc-200 dark:border-zinc-700
-    bg-white dark:bg-zinc-900 shadow-sm
-    hover:shadow-md hover:bg-zinc-50 dark:hover:bg-zinc-800
-    transition-all duration-200 ease-out
-    text-sm lg:h-24 lg:w-32 text-center flex-1"
+        class="group lg:p-3 p-1 lg:whitespace-normal whitespace-nowrap flex lg:flex-col flex-row items-center justify-center gap-1
+        rounded-lg border border-zinc-200 dark:border-zinc-700
+        bg-white dark:bg-zinc-900 shadow-sm
+        hover:shadow-md hover:bg-zinc-50 dark:hover:bg-zinc-800
+        transition-all duration-200 ease-out
+        text-sm lg:h-24 lg:w-32 text-center flex-1"
         :class="{
           'ring-2 ring-zinc-400 dark:ring-zinc-300': emojiStore.selectedGroup === group.key,
+          'select-none opacity-50': emojiStore.isLoading,
+          'cursor-pointer': !emojiStore.isLoading,
         }"
         @click="setGroup(group.key as '' | 'Smileys & Emotion' | 'People & Body' | 'Animals & Nature' | 'Food & Drink' | 'Travel & Places' | 'Activities' | 'Objects' | 'Symbols' | 'Flags')"
       >
--- pages/index.vue
+++ pages/index.vue
@@ -19,8 +19,8 @@ onMounted(async () => {
         hover-animation
       />
     </div>
-    <div v-else class="min-h-full min-w-full flex justify-center items-center">
-      <Loading />
+    <div v-else class="w-full h-full flex justify-center items-center">
+      <div class="h-12 w-12 rounded-full border-4 border-t-transparent border-b-transparent border-l-[#ffc337] border-r-[#ff66c4] animate-spin" />
     </div>
   </NuxtLayout>
 </template>
