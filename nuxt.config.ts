099b252359b7f915defc41e3f6591ab5bbdc0043
eiskaltDEV <atlasyigitaydin@gmail.com>
2025-04-18 17:24:10 +0300

    'deploy'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxt/ui', 'nuxt-gtag'],
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@pinia/nuxt', '@nuxt/ui', 'nuxt-gtag', '@nuxtjs/google-adsense'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
@@ -12,5 +12,8 @@ export default defineNuxtConfig({
   },
   gtag: {
     id: 'G-4R3XKFL775',
+  },
+  googleAdsense: {
+    id: 'ca-pub-1948589981733097',
   },
 })
